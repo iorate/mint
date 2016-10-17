20f45204276de8829ab905f4c952bb0e2bcbdfc8
iorate <iorate@users.noreply.github.com>
2016-10-17 22:27:54 +0900

    Change the license to Boost Software License

--- mint.cpp
+++ mint.cpp
@@ -1,3 +1,9 @@
+
+// Copyright iorate 2015.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include <exception>
 #include <iterator>
 #include <regex>
